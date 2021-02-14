5bfca6883ee415ef5265ed167b2dbb0c8b817511
shakibahammed <binshakibahammed2020@gmail.com>
2021-02-14 23:41:25 +0600

    completed all part

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ let sliders = [];
 // Find the name in the url and go to their website
 // to create your own api key
 const KEY = '15674931-a9d714b6e9d654524df198e00&q';
+// Enter button working part
+document.getElementById("search").addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    document.getElementById('search-btn').click();
+  }
+});
+
 
 // show images 
 const showImages = (images) => {
@@ -25,27 +32,33 @@ const showImages = (images) => {
     div.innerHTML = ` <img class="img-fluid img-thumbnail" onclick=selectItem(event,"${image.webformatURL}") src="${image.webformatURL}" alt="${image.tags}">`;
     gallery.appendChild(div)
   })
-
+  loadingSpinner()
 }
 
 const getImages = (query) => {
+  loadingSpinner()
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => showImages(data.hits))
     .catch(err => console.log(err))
+
 }
 
 let slideIndex = 0;
+
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
-
+  element.classList.toggle('added');
+  
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
   } else {
-    alert('Hey, Already added !')
+
+    sliders = sliders.filter(item => item.indexOf(img));
+    
   }
+ 
 }
 var timer
 const createSlider = () => {
@@ -68,13 +81,13 @@ const createSlider = () => {
   // hide image aria
   imagesArea.style.display = 'none';
   const duration = document.getElementById('duration').value || 1000;
-
-  if(document.getElementById('duration').value < 0) {
-    imagesArea.style.display ="block"
+  // checking duration negative
+  if (document.getElementById('duration').value < 0) {
+    imagesArea.style.display = "block"
     alert("Duration value can not negative")
-  
-  } 
-  else{
+
+  }
+  else {
     sliders.forEach(slide => {
       let item = document.createElement('div')
       item.className = "slider-item";
@@ -89,7 +102,7 @@ const createSlider = () => {
       changeSlide(slideIndex);
     }, duration);
   }
- 
+
 }
 
 // change slider index 
@@ -124,10 +137,15 @@ searchBtn.addEventListener('click', function () {
   const search = document.getElementById('search');
   getImages(search.value)
   sliders.length = 0;
+  
 })
 
 sliderBtn.addEventListener('click', function () {
   createSlider()
-
-
 })
+
+const loadingSpinner = () => {
+  const spinner = document.getElementById("loading-spinner");
+  spinner.classList.toggle('d-none');
+}
+
